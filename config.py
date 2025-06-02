d527bb11ec8386a2cf55a1a3c3fa38801fd6f713
owajbhai <owajshaikhrcm@gmail.com>
2025-06-02 06:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    API_ID = environ.get("API_ID", "23621595")
+    API_HASH = environ.get("API_HASH", "de904be2b4cd4efe2ea728ded17ca77d")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7736867939:AAGTuiBdoyM1zfs9alpLI51p5MiBhRUDqeA") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1249672673').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mongodb011:rxXV4pGzxLJgxaXQ@cluster0.undjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
