ebca7b2e66969de4eac5ac95535fb4182e3920f2
owajbhai <owajshaikhrcm@gmail.com>
2025-06-02 07:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002442237854'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")  # FORCE SUB ON - OFF
 
 
 class temp(object): 
